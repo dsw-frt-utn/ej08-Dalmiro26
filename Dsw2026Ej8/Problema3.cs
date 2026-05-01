f969829a8f498dfbba6945b1d19ae154a89a90cd
Dalmiro <DalmiroFerreyra@alu.frt.utn.edu.ar>
2026-05-01 00:23:52 -0300

    Se realizo el ejercicio numero 4 y tambien se realizo el ejercicio 3, nada mas que no lo pude terminar porque hay un problema con el parametro que tiene que recibir "Product product", me marca como error la clase Product.

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal partial class Problema3
+    {
+
+        public string CompararCopias(int originalValue, Product product)
+        {
+            int copia = originalValue;
+
+            copia++;
+
+            Product copiaProducto = product;
+
+            copiaProducto;
+
+            return $"{originalValue} - {copia} - {}";
+
+        }
+
+    }
+}
